8b44eca2db132028db4bacff50acc4faabd5bd4d
Gisela Chodos <gc@schawelcoles.com>
2017-10-05 13:35:49 -0600

    4_02

--- wp-content/themes/geesescores/inc/custom-header.php
+++ wp-content/themes/geesescores/inc/custom-header.php
@@ -23,9 +23,9 @@
 function geesescores_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'geesescores_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => 'ffffff',
+		'width'                  => 2000,
+		'height'                 => 850,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'geesescores_header_style',
 	) ) );
