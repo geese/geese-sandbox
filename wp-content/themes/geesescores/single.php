0eb5a1df4da7b5284943d9b83dad78bc950e4708
Gisela Chodos <gc@schawelcoles.com>
2017-10-10 09:41:02 -0600

    7_04 Use Flexbox to create a responsive layout

--- wp-content/themes/geesescores/single.php
+++ wp-content/themes/geesescores/single.php
@@ -6,8 +6,7 @@
  *
  * @package Geesescores
  */
-get_header();
-?>
+get_header(); ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
@@ -16,8 +15,7 @@ get_header();
         while (have_posts()) : the_post();
 
             get_template_part('template-parts/content', 'single');  //used to be get_post_format() instead of 'single'.  '-single' will be added to template-parts/content.
-
-            
+           
 
         endwhile; // End of the loop.
         ?>
@@ -25,5 +23,4 @@ get_header();
     </main><!-- #main -->
 </div><!-- #primary -->
 
-<?php
-get_footer();
+<?php get_footer();
